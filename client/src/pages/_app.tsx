3e2e3e3310a83369a249c542f3f2904662f69e58
beatrizlana <beatriz.lana@stoodi.com.br>
2023-09-19 16:50:20 -0300

    style: fix linter issues

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,15 @@
+import { AppProps } from 'next/app'
 import {ThemeProvider} from 'styled-components'
 import theme from '@/styles/theme'
+import GlobalStyles from '@/styles/global'
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={theme}>
+			<GlobalStyles />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	)
 }
+
+export default App
