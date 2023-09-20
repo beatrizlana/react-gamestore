af04a7871a4110cc4c10a394411e60ed4cc5c4c9
beatrizlana <beatriz.lana@stoodi.com.br>
2023-09-20 17:13:38 -0300

    feat(logo): create logo styles, tests, story, and structure

--- client/src/components/Logo/styles.ts
+++ client/src/components/Logo/styles.ts
@@ -1,9 +1,38 @@
 import styled, {css} from 'styled-components'
 import {LogoProps} from '.'
+import media from 'styled-media-query'
 
-// ! is a non-null assertion operator (its default value is being passed on component declaration)
+const wrapperModifiers = {
+	md: () => css`
+		width: 11rem;
+		height: 3.3rem;
+	`,
+	lg: () => css`
+		width: 20rem;
+		height: 6rem;
+	`,
+	hideText: () => css`
+		${media.lessThan('medium')`
+			width: 5.8rem;
+      height: 4.5rem;
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+      .logo__text {
+        display: none;
+      }
+		`}
+	`
+}
+
 export const Wrapper = styled.div<LogoProps>`
-	${({theme, color}) => css`
+	${({theme, color, size, hideText}) => css`
+		// ! is a non-null assertion operator (its default value is being passed on component declaration)
 		color: ${theme.colors[color!]};	
+
+		// !! operator prevents the value from being null or undefined converting it to a boolean
+		${!!size && wrapperModifiers[size]}
+		${!!hideText && wrapperModifiers.hideText}
 	`}
 `
--- client/src/components/Logo/test.tsx
+++ client/src/components/Logo/test.tsx
@@ -1,13 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from '.'
+import 'jest-styled-components'
+
+import {renderWithTheme} from 'utils/test/helpers'
+import theme from 'styles/theme'
 
 describe('<Logo />', () => {
-	it('should render the heading', () => {
-		const { container } = render(<Logo />)
+	it('should render white label by default', () => {
+		renderWithTheme(<Logo />)
 
-		expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+		expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+			color: theme.colors.white
+		})
+	})
+	it('shouldn\'t render other colors than black or white', () => {
+		renderWithTheme(<Logo color="red" />)
 
-		expect(container.firstChild).toMatchSnapshot()
+		expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+			color: theme.colors.white
+		})
+	})
+	it('should render medium size by default', () => {
+		renderWithTheme(<Logo />)
+		expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+			width: '11rem'
+		})
+	})
+	it('should render logo without text on mobile devices', () => {
+		renderWithTheme(<Logo hideText />)
+		expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyleRule(
+			'width', '5.8rem', {
+				media: '(max-width: 768px)'
+			}
+		)
 	})
 })
